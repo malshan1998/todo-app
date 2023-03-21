7e02d4626d156d6625613afaa4e1ef84af21a4e4
malshan1998 <malshanranawella@gmail.com>
2023-03-21 07:41:41 +0530

    Removed unnecessary comment and exception

--- src/main/java/io/malshan/programming/todoapp/TodoAppApplication.java
+++ src/main/java/io/malshan/programming/todoapp/TodoAppApplication.java
@@ -9,12 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//	Video of reference: https://www.youtube.com/watch?v=XKiYaHJhEPI&t=120s
-
-	//	Access Swagger from: http://localhost:8080/swagger-ui/
-	//	Access H2 console from: http://localhost:8080/h2_console
-		//	Use the spring.datasource.url from application.properties as the JDBC URL
-
 @SpringBootApplication
 public class TodoAppApplication implements CommandLineRunner {
 
@@ -27,7 +21,7 @@ public class TodoAppApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		Users user1 = new Users();
 		user1.setUsername("John");
 		user1.setPassword("Should be hashed");
